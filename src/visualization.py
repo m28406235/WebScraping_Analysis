6c4733284f74477ce0cf49943f2faf74f2e64223
m28406235 <m28406235@gmail.com>
2025-05-01 22:28:30 +0300

    Refactor visualization: Enhance scatter plot to highlight best-performing phones and improve hover information

--- src/visualization.py
+++ src/visualization.py
@@ -62,26 +62,21 @@ phone_data["phone_tier"] = pd.Categorical(phone_data["phone_tier"], categories=t
 
 #1
 # Scatter Plot for Performance vs Price
-fig = px.scatter(
-    phone_data,
-    x='price',
-    y='antutu_score',
-    color='phone_tier',
-    hover_name='phone_name',
-    title="Performance vs Price",
-    labels={
-        'price': 'Price (USD)',
-        'antutu_score': 'Antutu Score (Performance)'
-    },
-    category_orders={"phone_tier": ["Budget", "Mid-Range", "Premium", "Flagship"]}  # Confirm order
-)
+tier_order = ['Budget', 'Mid-Range', 'Premium', 'Flagship']
+phone_data['phone_tier'] = pd.Categorical(phone_data['phone_tier'], categories=tier_order, ordered=True)
 
-fig.update_layout(
-    xaxis_title="Price (USD)",
-    yaxis_title="Antutu Score (Performance)",
-    template="plotly_white"
+phone_data['text'] = phone_data['phone_name']
 
-)
+best_phones_idx = phone_data.groupby('phone_tier').apply(lambda group: (group['antutu_score'] / group['price']).idxmax())
+best_phones_data = phone_data.loc[best_phones_idx]
+
+fig = px.scatter(phone_data, x='price', y='antutu_score', color='phone_tier', category_orders={'phone_tier': tier_order}, color_discrete_map={'Budget': '#2ca02c', 'Mid-Range': '#1f77b4', 'Premium': '#9467bd', 'Flagship': '#d62728'}, hover_data={'text': True, 'price': True, 'antutu_score': False, 'phone_tier': False})
+
+fig.add_scatter(x=best_phones_data['price'], y=best_phones_data['antutu_score'], mode='markers', marker=dict(symbol='diamond', size=10, color='gold', line=dict(width=2, color='black')), name="Best Phones", hovertemplate='%{customdata[0]}<br>Price: %{x}<extra></extra>', customdata=best_phones_data[['text']])
+
+fig.update_traces(hovertemplate='%{customdata[0]}<br>Price: %{x}<extra></extra>', customdata=phone_data[['text']], marker=dict(size=10), selector=dict(mode='markers'))
+
+fig.update_layout(title="Price vs Antutu Score", xaxis_title="Price", yaxis_title="Antutu Score", legend_title="Phone Tier", hovermode='closest')
 
 fig.show()
 #2
