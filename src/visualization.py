72ab37e0e56dc40130cd7bcd3c8efab8c15a32e1
Amr Mohamed <amrm08018@gmail.com>
2025-05-01 20:26:33 +0300

    Visuallization

--- src/visualization.py
+++ src/visualization.py
@@ -41,7 +41,7 @@ phone_data["phone_tier_encoded"] = encoder.fit_transform(phone_data["phone_tier"
 features = ["price", "phone_tier_encoded", "antutu_score"]
 
 #knn imputation
-knn_imputer = KNNImputer(n_neighbors=5)  # استخدم 5 أقرب جيران
+knn_imputer = KNNImputer(n_neighbors=5)  # use 5 nearest neighbors
 imputed_data = knn_imputer.fit_transform(phone_data[features])
 
 #update the original DataFrame with imputed values
@@ -277,34 +277,3 @@ fig.update_layout(
 )
 
 fig.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# tier_order = ['Budget', 'Mid-Range', 'Premium', 'Flagship']
-# phone_data['phone_tier'] = pd.Categorical(phone_data['phone_tier'], categories=tier_order, ordered=True)
-#
-# phone_data['text'] = phone_data['phone_name']
-#
-# best_phones_idx = phone_data.groupby('phone_tier').apply(lambda group: (group['antutu_score'] / group['price']).idxmax())
-# best_phones_data = phone_data.loc[best_phones_idx]
-#
-# fig = px.scatter(phone_data, x='price', y='antutu_score', color='phone_tier', category_orders={'phone_tier': tier_order}, color_discrete_map={'Budget': '#2ca02c', 'Mid-Range': '#1f77b4', 'Premium': '#9467bd', 'Flagship': '#d62728'}, hover_data={'text': True, 'price': True, 'antutu_score': False, 'phone_tier': False})
-#
-# fig.add_scatter(x=best_phones_data['price'], y=best_phones_data['antutu_score'], mode='markers', marker=dict(symbol='diamond', size=10, color='gold', line=dict(width=2, color='black')), name="Best Phones", hovertemplate='%{customdata[0]}<br>Price: %{x}<extra></extra>', customdata=best_phones_data[['text']])
-#
-# fig.update_traces(hovertemplate='%{customdata[0]}<br>Price: %{x}<extra></extra>', customdata=phone_data[['text']], marker=dict(size=10), selector=dict(mode='markers'))
-#
-# fig.update_layout(title="Price vs Antutu Score", xaxis_title="Price", yaxis_title="Antutu Score", legend_title="Phone Tier", hovermode='closest')
-#
-# fig.show()
